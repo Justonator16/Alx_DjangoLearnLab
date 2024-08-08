5da4e029654146eee3b1f276cf930f5296d4d89c
Junior <juniorjusto16@gmail.com>
2024-08-08 19:19:27 +0200

    Admin model changed

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Book
 
+class BookAdmin(admin.ModelAdmin):
+    fields = ["title","author","publication_year"]
 # Register your models here.
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
