434a6a627c1082226d7c5fd4e88f9f86e495f4cb
Junior <juniorjusto16@gmail.com>
2024-08-08 19:38:27 +0200

    List filters added to admin.py

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -4,6 +4,7 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     fields = ["title","author","publication_year"]
     list_filter = ["author","title","publication_year"]
-    
+    search_fields = []
+
 # Register your models here.
 admin.site.register(Book, BookAdmin)
