214512f6a0555d6cdf9b6fc0ed216864df93737f
Junior <juniorjusto16@gmail.com>
2024-08-23 20:38:59 +0200

    Added exception

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpReponse
 from models import Book
 
 # Create your views here.
 def list_books(request):
     book_list = Book.objects.all() 
     if not book_list:
-        raise
+        raise_exception = "Error occured"
+        return HttpReponse(raise_exception)
 
     return render(request, 'relationship_app/list_books.html' , context={'books': book_list})
