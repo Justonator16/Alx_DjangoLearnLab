0fc9400851ed44e8057491edddf0af34b8ccc85c
Junior <juniorjusto16@gmail.com>
2024-08-23 22:06:41 +0200

    added views function

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class BookForm(forms.Form):
+class ExampleForm(forms.Form):
     title = forms.CharField(max_length=100)
     author = forms.CharField(max_length=50)
     publication_year = forms.DateField()
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpReponse
 from models import Book
+from .forms import ExampleForm
 
 # Create your views here.
 def list_books(request):
@@ -11,3 +12,16 @@ def list_books(request):
         response["Content-Security-Policy"] = "default-src 'self';"
 
     return render(request, 'relationship_app/list_books.html' , context={'books': book_list})
+
+def form_example(request):
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+
+        if form.is_valid() == True:
+            form.save()
+            return render(request, "bookshelf/book_list.html")
+        else:
+            return HttpReponse("An error occured Invalid Input.")
+    else:
+        form = ExampleForm()
+        return render(request, "bookshelf/form_example.html", {"form": form})
