b2abe367cddfbc2e314da7b7595c817e7384392c
Junior <juniorjusto16@gmail.com>
2024-08-27 22:36:36 +0200

    added auth token

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import BookList, BookViewSet
+from .views import BookList, BookViewSet, CustomAuthToken, BookCreateAPIView
 from rest_framework.routers import DefaultRouter
+
 
 router = DefaultRouter()
 router.register(r'my-models', BookViewSet)
@@ -8,4 +9,7 @@ router.register(r'my-models', BookViewSet)
 urlpatterns = [
     path("", BookList.as_view(), name="book_list"),
     path("api/", include(router.urls)),
+    path('api-token-auth', CustomAuthToken.as_view(), name='api_token_auth'),
+    path("books/", BookCreateAPIView.as_view(), name="books-list-create"),
+
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,13 +1,49 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer
 
 # Create your views here.
 class BookList(generics.ListAPIView):
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        queryset = Book.objects.all()
+        serializer = BookSerializer(queryset, many=True)
+
+        return Response(serializer.data)
+
+class BookCreateAPIView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
+
+    def post(self , request):
+        serializer = BookSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        response = super().postpost(self, request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+
+        return Response({
+            'token': token.key,
+            'user_id': token.user_id,
+            'email': token.user.email
+        })
