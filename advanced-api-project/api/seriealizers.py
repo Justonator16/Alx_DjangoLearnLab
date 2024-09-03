315272ab3c4e8405550eea15e70b02f0b4da1087
Junior <juniorjusto16@gmail.com>
2024-09-03 14:13:26 +0200

    rename serializers

--- advanced-api-project/api/seriealizers.py
+++ advanced-api-project/api/seriealizers.py
@@ -3,7 +3,7 @@ from .models import Author, Book
 import datetime as  dt
 
 # model serializers user fields from models as input to convert into a json, xml
-class BookSerializers(serializers.ModelSerializers):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
@@ -14,7 +14,7 @@ class BookSerializers(serializers.ModelSerializers):
             raise serializers.ValidationError(f"Year must be <= {current_year}")
         return data
 
-class AuthorSerializers(serializers.ModelSerializers):
+class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = ['name']
