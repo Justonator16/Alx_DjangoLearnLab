a63ab5d1ed43ca7e5a56c2aa92566dc50396383d
Junior <juniorjusto16@gmail.com>
2024-09-04 12:20:31 +0200

    Fixed checker issues

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,41 +1,42 @@
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Book
 from .forms import BookForm
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .seriealizers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 
 #Provides a list of all book created from models
-class BookListView(IsAuthenticatedOrReadOnly, generic.ListView):
+class BookListView(IsAuthenticatedOrReadOnly, ListView):
     template_name = 'api/book_list.html'
     model = Book
     context_object_name = "books"
 
 # Returns a detailed book attributes
-class BookDetailView(IsAuthenticatedOrReadOnly, generic.DetailView):
+class BookDetailView(IsAuthenticatedOrReadOnly, DetailView):
     template_name = 'api/book_detail.html'
     model = Book
     context_object_name = "book"
 
 #Create a book but only logged in users are allowed
-class BookCreateView(IsAuthenticated , generic.CreateView):
+class BookCreateView(IsAuthenticated , CreateView):
     template_name = 'api/book_create.html'
     model = Book
     form_class = BookForm    
     success_url = reverse_lazy('book_list')
 
 # Only loggedin users are allowed to update a book
-class BookUpdateViews(IsAuthenticated , generic.UpdateView):
+class BookUpdateViews(IsAuthenticated , UpdateView):
     template_name = 'api/book_update.html'
     model = Book
     form_class = BookForm
     success_url = reverse_lazy('book_list')
 
 #Only logged in users are allowed to delete books
-class BookDeleteView(IsAuthenticated, generic.DeleteView):
+class BookDeleteView(IsAuthenticated, DeleteView):
     template_name = 'api/book_delete.html'
     model = Book
     context_object_name = "book"
@@ -45,7 +46,7 @@ class BookDeleteView(IsAuthenticated, generic.DeleteView):
 API SECTION
 """
 
-class BookListAPIView(ListAPIView):
+class BookListAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
