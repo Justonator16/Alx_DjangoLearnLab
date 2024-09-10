7969db86c1f51271d9b849a1d59cd606b8d7069c
Junior <juniorjusto16@gmail.com>
2024-09-10 21:30:01 +0200

    checker fix

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_blog.wsgi.application'
 LOGIN_REDIRECT_URL = 'blog/'
+LOGOUT_REDIRECT_URL = 'login/'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
