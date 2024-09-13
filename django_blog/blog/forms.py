d6553d632a78f10b7e762f04213251c70e2012ed
Justonator16 <131472241+Justonator16@users.noreply.github.com>
2024-09-13 11:02:32 +0000

    debug

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
 from .models import Post, Comment
+from taggit.forms import TagWidget()
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -19,6 +20,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'author', 'tags']
+        widgets = {
+            'tags': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add tags here'}),
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
